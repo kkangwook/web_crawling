0ca4de23bb4a3e742e69c58143a1534467f8b999
kkangwook <send2woohyun@naver.com>
2025-03-17 17:29:45 +0900

    Update 5_beautifulsoup_csv.py

--- crawling/5_beautifulsoup_csv.py
+++ crawling/5_beautifulsoup_csv.py
@@ -8,7 +8,7 @@ filename='시가총액.csv'   #미리 파일만듬
 f=open(filename,'w',encoding='utf-8-sig',newline='') #newline공백으로 해서 줄 사이사이에 공백줄있는거 없앰, 파일생성
 writer=csv.writer(f) # f파일에 csv로 저장할수있게하는 객체를 생성
 title='N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE	토론실'.split('\t')
-writer.writerow(title)  #처음에 컬럼명에 title값 집어넣음
+writer.writerow(title)  #처음에 컬럼명에 title값 집어넣음, 리스트 각 요소가 각 속성값에 들어감
 
 for i in range(1,11):
     url=f'https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page={i}'
