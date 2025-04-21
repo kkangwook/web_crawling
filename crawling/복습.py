45ad5c34c6d1065274a2d79a4133201166985084
kkangwook <send2woohyun@naver.com>
2025-04-21 10:03:01 +0900

    Update 복습.py

--- crawling/복습.py
+++ crawling/복습.py
@@ -1,4 +1,4 @@
 1. beautifulsoup의 find로 축구팀 리스트 가져오기
-2. beautifulsoup의 select로 한번에 탑뉴스리스트 전부->여기서 링크 추출
+2. beautifulsoup의 select로 계층적 선택자 이용해 한번에 탑뉴스리스트 전부->여기서 링크 추출
 3. beautifulsoup으로 1~10페이지의 극내 시총 주식정보 스크래핑 후 csv로 저장
-4. selenium으로 query입력받은 다음 네이버 홈페이지에 입력 후 이미지로 이동 후 상단 10개 이미지 저장하기
+4. selenium으로 query입력받은 다음 네이버 홈페이지에 입력 후 이동->뒤로->앞으로->새로고침-> 이미지로 이동 후 상단 10개 이미지 저장하기
