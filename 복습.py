0b87990bb2d9f039fb08c774645565e2ae7286ac
kkangwook <send2woohyun@naver.com>
2025-04-30 13:13:42 +0900

    Update 복습.py

--- 복습.py
+++ 복습.py
@@ -6,10 +6,14 @@
 
 
 # flask
+- itwill/textmining/workspace/flask의 flask_practice.py에 작성
 1. 홈페이지 제작-> 탭에 myhomepage, 화면에 큰 'homepage'링크텍스트(클릭하면 그대로 홈페이지로), html~css~javascript링크, create링크 , 'db로 이동' 링크
 2. html~css~javascript클릭하면 body로 이동하며 title과 밑에 내용있음, create링크, update링크, delete링크, db로 이동 링크
 3. create flask생성 -> text와 textarea로 입력받아 새로운 데이터 생성
 4. update flask생성 -> text와 textarea로 입력받아 기존 데이터 변경
 5. delete flask생성 -> 버튼으로 입력받아 클릭하면 해당 데이터 삭제
-6. db로 이동 링크 클릭하면 로그인 창 뜸 -> user='c##scott',password='tiger' ->제대로 되면 db로/ 잘못되면 '잘못된 계정' 출력후 홈페이지와 로그인 링크 두 종류 제공
-7. db로 이동 시 select입력으로 emp의 ename값과 보고싶은 열 데이터 두가지 입력받음->클릭시 해당 데이터 보여줌
+6. db로 이동 링크 클릭하면 로그인 창 뜸 -> user='scott',password='tiger' ->제대로 되면 /database/로 잘못되면 /error/ '잘못된 계정' 출력후 홈페이지와 로그인 링크 두 종류 제공
+7. /database/로 이동 시 select입력으로 emp의 ename값을 입력->클릭시 /emp/로 이동
+8. 이 /emp/는 templates안의 practice.html로 불러와 jinja2형식 사용-> for문과 if문 사용해 입력받은 이름은 'ename의 월급은 sal원입니다 볼드체로 출력'
+->그 밑에 static/images에서 홍길동 사진 가져옴-> 그 외의 선택받지 않은 ename들은 '누군가의 월급은 sal원'으로 리스트로 모두 출력
+->다른정보보기 링크로 /database/로 이동하는 링크 생성
